e60394b16187dae2f4d8909de0a89bae0d775d78
cqlijingwei <cqlijingwei@126.com>
2018-09-11 10:35:55 -0400

    Add files via upload

---
+++ projects/mysite/conn/conn.php
@@ -0,0 +1,5 @@
+<?php
+    $conn=mysqli_connect("localhost","root","root","db_shop") or die("The database server connection error
+".mysqli_error());
+    mysqli_query($conn,"set names gb2312");
+?>
